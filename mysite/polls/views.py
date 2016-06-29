9b57e79f4b95d12514768a443b98413f001c7278
sztyui <6323pityu@citromail.hu>
2016-06-29 16:46:43 +0200

    https://docs.djangoproject.com/en/1.9/intro/tutorial05/#writing-your-first-django-app-part-5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,21 +4,43 @@ from django.template import loader
 from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.db.models import F
+from django.views import generic
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
+# N.U. function
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
   context = { 'latest_question_list' : latest_question_list }
   return render(request, 'polls/index.html', context)
-  
+
+# N.U. function
 def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', { 'question' : question })
-  
+
+# N.U. function
 def results(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/results.html', {'question' : question})
+"""
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
@@ -33,5 +55,4 @@ def vote(request, question_id):
   else:
     selected_choice.votes = F('votes') + 1
     selected_choice.save()
-    
     return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
